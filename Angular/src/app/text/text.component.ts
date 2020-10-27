48a42d8f35325d14ddc7afb60878693b903a13e8
Unknown <y.creak@hotmail.com>
2020-10-27 23:01:26 +0100

    Can now choose texts
    Also better Greek support in the database, Lysias added.

--- Angular/src/app/text/text.component.ts
+++ Angular/src/app/text/text.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { UtilityService } from '../utility.service';
 import { AuthService } from '../auth/auth.service';
+import {MatDialog} from '@angular/material/dialog';
+import { TemplateRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-text',
@@ -9,23 +11,45 @@ import { AuthService } from '../auth/auth.service';
   styleUrls: ['./text.component.scss']
 })
 export class TextComponent implements OnInit {
-
+  @ViewChild('CallBookSelect') CallBookSelect: TemplateRef<any>;
+  authorsJSON;
+  booksJSON;
   // Components that hold the text and commentary
   T_Text;
   T_TextCommentary
 
   // Currently selected book and line
   currentBook : number = 1;
+  currentBookTitle : string;
   selectedLine : number;
 
   constructor(
     private api: ApiService,
     private utility: UtilityService,
     public authService: AuthService,
+    private dialog: MatDialog, 
+
     ) { }
 
   ngOnInit(): void {
+    this.api.GetAuthors().subscribe(data => this.authorsJSON = data);
     this.RequestText(1)
+  }
+
+  public RequestBooks(author: number){
+    this.api.GetBooks(author).subscribe(
+      data => {
+        this.booksJSON = data;
+        console.log(data)
+        this.currentBook = data[0].id;
+        this.currentBookTitle = data[0].title
+      }
+    );      
+  }
+
+  // Opens dialog to select a new book
+  public OpenBookSelect() {
+    let dialogRef = this.dialog.open(this.CallBookSelect); 
   }
 
   public RequestText(book: number){
