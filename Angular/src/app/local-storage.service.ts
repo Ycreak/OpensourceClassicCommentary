f78a299bb5ce0f683aedd3ddf72b1c679cdbd436
Sebastiaan van Wijk <sebas-vanwijk@outlook.com>
2023-05-09 23:47:38 +0200

    Created a storage service

---
+++ Angular/src/app/local-storage.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocalStorageService {
+  constructor() {}
+
+  public saveData(key: string, value: string): void {
+    console.log("save func called!", key, value)
+    localStorage.setItem(key, value);
+  }
+
+  public getData(key: string): any {
+    return localStorage.getItem(key);
+  }
+
+  public removeData(key: string): void {
+    localStorage.removeItem(key);
+  }
+
+  public clearData(): void {
+    localStorage.clear();
+  }
+}
