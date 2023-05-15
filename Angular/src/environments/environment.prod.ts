2e71a6231f480487f84f2470163958e43129691d
Ycreak <y.creak@hotmail.com>
2023-05-15 16:47:56 +0200

    Revert "Favicon (#125)" (#126)
    This reverts commit e6bf9aab7842ba332924986429fd662c424c816a.

--- Angular/src/environments/environment.prod.ts
+++ Angular/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   title: 'Open Source Classics Commentary',
-  short_title: 'OSCC',
+  short_title: 'OSCC (Dev)',
   dashboard_id: 255, 
   referencer_id: 256, 
   playground_id: 0,
-  flask_api: 'https://oscc.nolden.biz:5004/',
+  flask_api: 'https://oscc.nolden.biz:5003/',
   is_logged_in: false,
   current_user_name: '',
   current_user_role: 'guest',
