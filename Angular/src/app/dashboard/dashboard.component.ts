94f52150b118a35ea5d8b1a18d5db6398b192346
Ycreak <y.creak@hotmail.com>
2023-05-15 15:12:16 +0200

    Metrical analysis + summaries (#119)
    * Metrical analysis + summaries
    
    * Dev aware title, better observables from api (#120)
    
    * Dev aware title, better observables from api
    
    * Dashboard properly cleared when retrieving new data (#121)
    
    * Dashboard properly cleared when retrieving new data
    
    * reset_fragment_form comment rewording
    
    * Playground fix (#122)
    
    * Made linter happy
    
    * Fixed small api issue
    
    * Delete fragment in playground no longer able to delete everything
    
    * Notes now also fixed, thx seb
    
    ---------
    
    Co-authored-by: Luuk Nolden <luuk.nolden@naturalis.nl>
    
    ---------
    
    Co-authored-by: Luuk Nolden <luuk.nolden@naturalis.nl>
    Co-authored-by: Sebastiaan van Wijk <sebas-vanwijk@outlook.com>
    
    ---------
    
    Co-authored-by: Luuk Nolden <luuk.nolden@naturalis.nl>
    Co-authored-by: Sebastiaan van Wijk <sebas-vanwijk@outlook.com>
    
    ---------
    
    Co-authored-by: Luuk Nolden <luuk.nolden@naturalis.nl>
    Co-authored-by: Sebastiaan van Wijk <sebas-vanwijk@outlook.com>

--- Angular/src/app/dashboard/dashboard.component.ts
+++ Angular/src/app/dashboard/dashboard.component.ts
@@ -92,6 +92,7 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit {
     differences: new FormControl(''),
     commentary: new FormControl(''),
     apparatus: new FormControl(''),
+    metrical_analysis: new FormControl(''),
     reconstruction: new FormControl(''),
     // This array is dynamically filled by the function push_fragment_context_to_fragment_form().
     // It will contain multiple FormGroups per context, containing an author, location and text.
@@ -161,7 +162,7 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit {
     this.expand_user_table_row();
 
     /** Handle what happens when new fragment names arrive */
-    this.fragment_names_subscription = this.api.new_fragment_names_alert.subscribe((column_id) => {
+    this.fragment_names_subscription = this.api.new_fragment_names_alert$.subscribe((column_id) => {
       if (column_id == environment.dashboard_id) {
         this.selected_fragment_data.fragment_names = this.api.fragment_names;
       } else if (column_id == environment.referencer_id) {
@@ -170,8 +171,9 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit {
     });
 
     /** Handle what happens when new fragments arrive */
-    this.fragments_subscription = this.api.new_fragments_alert.subscribe((column_id) => {
+    this.fragments_subscription = this.api.new_fragments_alert$.subscribe((column_id) => {
       if (column_id == environment.dashboard_id) {
+        this.reset_fragment_form();
         this.convert_Fragment_to_fragment_form(this.api.fragments[0]);
         // Set the data for the drop down menus
         this.selected_fragment_data.author = this.fragment_form.value.author;
@@ -258,6 +260,7 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit {
       'commentary',
       'apparatus',
       'reconstruction',
+      'metrical_analysis',
       'status',
       'lock',
       'published',
@@ -437,7 +440,7 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit {
   protected reset_fragment_form(): void {
     // First, remove all data from the form
     this.fragment_form.reset();
-    // Second, remove the controls created for the FormArrays
+    // Second, remove the previously created FormArray controls and set new ones
     this.fragment_form.setControl('context', new FormArray([]));
     this.fragment_form.setControl('lines', new FormArray([]));
     this.fragment_form.setControl('linked_fragments', new FormArray([]));
--- Angular/src/environments/environment.prod.ts
+++ Angular/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
+  title: 'Open Source Classics Commentary',
+  short_title: 'OSCC (Dev)',
   dashboard_id: 255, 
   referencer_id: 256, 
   playground_id: 0,
@@ -7,14 +9,4 @@ export const environment = {
   current_user_name: '',
   current_user_role: 'guest',
   production: true,
-  firebase: {
-    apiKey: 'AIzaSyC1D1PH6p1xvOzK6CBWwM1IdWyIiXsGBd4',
-    authDomain: 'oscc-4c70c.firebaseapp.com',
-    databaseURL: 'https://oscc-4c70c.firebaseio.com',
-    projectId: 'oscc-4c70c',
-    storageBucket: 'oscc-4c70c.appspot.com',
-    messagingSenderId: '454080481044',
-    appId: '1:454080481044:web:90e431e9a49abac872e401',
-    measurementId: 'G-JWE82D6XRQ',
-  },
 };
