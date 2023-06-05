274a5409fefc4157891b609bd512204b3f8f8f07
Sebastiaan van Wijk <sebas-vanwijk@outlook.com>
2023-06-05 13:15:03 +0200

    Changes based on code review #1

--- Angular/src/app/models/Introduction_form.ts
+++ Angular/src/app/models/Introduction_form.ts
@@ -1,15 +1,11 @@
 export class Introduction_form {
-    constructor ( introduction? : Partial<Introduction_form>){
-        // Allow the partial initialisation of the object
-        Object.assign( this, introduction );  
-    }
+  constructor(introduction?: Partial<Introduction_form>) {
+    // Allow the partial initialisation of the object
+    Object.assign(this, introduction);
+  }
 
-    author = '';
-    title = '';
-    author_introduction_text = '';
-    title_introduction_text = '';
-
-    // TODO: Comments
-    retrieved_authors: string[] = [];
-    retrieved_titles: string[] = [];
+  author = '';
+  title = '';
+  author_introduction_text = '';
+  title_introduction_text = '';
 }
