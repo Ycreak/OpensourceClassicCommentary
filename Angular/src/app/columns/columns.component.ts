1989f28ddff55b0e95e83a2e77c329fe7dc98a07
Sebastiaan van Wijk <sebas-vanwijk@outlook.com>
2023-07-28 20:12:45 +0200

    Context menu copy

--- Angular/src/app/columns/columns.component.ts
+++ Angular/src/app/columns/columns.component.ts
@@ -1,5 +1,5 @@
 // Library imports
-import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewEncapsulation, ViewChild } from '@angular/core';
 import { Output, EventEmitter } from '@angular/core';
 import { trigger, transition, style, animate } from '@angular/animations';
 import { MatDialog } from '@angular/material/dialog';
@@ -17,6 +17,7 @@ import { DocumentFilterComponent } from '@oscc/dialogs/document-filter/document-
 // Model imports
 import { Fragment } from '@oscc/models/Fragment';
 import { Column } from '@oscc/models/Column';
+import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-columns',
@@ -270,4 +271,34 @@ export class ColumnsComponent implements OnInit, OnDestroy {
       },
     });
   }
+
+    /** 
+   * Method called when the user click with the right button
+   * Used to open a context menu as described in this component's html 
+   * @param event MouseEvent, it contains the coordinates 
+   * @param item Our data contained in the row of the table 
+   * @author sajvanwijk
+   */ 
+    // we create an object that contains coordinates 
+    menuTopLeftPosition =  {x: '0', y: '0'} 
+ 
+    // reference to the MatMenuTrigger in the DOM 
+    @ViewChild(MatMenuTrigger, {static: true}) matMenuTrigger: MatMenuTrigger; 
+
+    protected onRightClick(event: MouseEvent, item) { 
+      // preventDefault avoids to show the visualization of the right-click menu of the browser 
+      event.preventDefault(); 
+ 
+      // we record the mouse position in our object 
+      this.menuTopLeftPosition.x = event.clientX + 'px'; 
+      this.menuTopLeftPosition.y = event.clientY + 'px'; 
+ 
+      // we open the menu 
+      // we pass to the menu the information about our object 
+      this.matMenuTrigger.menuData = {item: item} 
+ 
+      // we open the menu 
+      this.matMenuTrigger.openMenu(); 
+ 
+  } 
 }
