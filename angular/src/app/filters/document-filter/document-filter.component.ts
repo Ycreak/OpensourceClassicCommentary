c80fa214e36426a09f9582c909ecd74d3bf7b693
Luuk Nolden <y.creak@hotmail.com>
2024-02-09 16:18:50 +0100

    Advanced filter done for fragments

--- angular/src/app/filters/document-filter/document-filter.component.ts
+++ angular/src/app/filters/document-filter/document-filter.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {ColumnsService} from '@oscc/columns/columns.service';
 import {FilterService} from './filter.service';
 
 @Component({
@@ -9,12 +10,13 @@ import {FilterService} from './filter.service';
   styleUrls: ['./document-filter.component.scss'],
 })
 export class DocumentFilterComponent {
-  private column_id: string;
+  private column_id: number;
 
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data: any,
-    public dialogRef: MatDialogRef<DocumentFilterComponent>, 
-    private filter: FilterService
+    @Inject(MAT_DIALOG_DATA) private data: any,
+    private dialogRef: MatDialogRef<DocumentFilterComponent>, 
+    private columns: ColumnsService,
+    private filter: FilterService,
   ) {
     this.column_id = data.column_id;
   }
@@ -24,13 +26,29 @@ export class DocumentFilterComponent {
   }
 
   protected add_table(): void {
-    // We now request the api to add the selected documents to the column we have been provided
-    this.dialogRef.close(this.filter.dataSource.filteredData);
+    // We now request the api to add the documents from the table to the column we have been provided
+    // The table data contains objects, which we can use as filters for the database.
+    const filters = this.filter.dataSource.filteredData;
+    if(filters.length){
+      //TODO: for now, we need to request every single document from the server.
+      // New API update will allow us to request a list of filters
+      filters.forEach((doc: any) => {
+        this.columns.request(doc, this.column_id, true);
+      })
+    } 
   }
 
   protected add_selection(): void {
     // We now request the api to add the selected documents to the column we have been provided
-    this.dialogRef.close(this.filter.selection.selected);
+    // The selection contains objects, which we can use as filters for the database.
+    const filters = this.filter.selection.selected;
+    if(filters.length){
+      //TODO: for now, we need to request every single document from the server.
+      // New API update will allow us to request a list of filters
+      filters.forEach((doc: any) => {
+        this.columns.request(doc, this.column_id, true);
+      })
+    } 
   }
 
 }
