96b1d44504db7b54bfc86d3dd6ac830b5ea5b3bf
Luuk Nolden <y.creak@hotmail.com>
2024-06-12 20:55:05 +0200

    Fixed problems with the bibliography

--- angular/src/app/commentary/commentary.component.ts
+++ angular/src/app/commentary/commentary.component.ts
@@ -115,7 +115,7 @@ export class CommentaryComponent {
    * @author Ycreak
    */
   private create_bibliography(doc: any): string {
-    return this.bib.convert_keys_into_bibliography(doc.commentary.bib_keys);
+    return this.bib.convert_keys_into_bibliography(this.bib.get_keys_from_commentary(doc.commentary));
   }
 
   /**
