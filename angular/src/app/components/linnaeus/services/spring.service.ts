abbf1eaf7b6abdeb75537b11089c9b98e4a2449d
Ycreak <y.creak@hotmail.com>
2025-02-27 22:07:55 +0100

    Linnaeus test (#319)
    * Linnaeus test
    
    * Linnaeus
    
    * Update
    
    * Remove test
    
    ---------
    
    Co-authored-by: Luuk Nolden <luuk.nolden@naturalis.nl>

---
+++ angular/src/app/components/linnaeus/services/spring.service.ts
@@ -0,0 +1,43 @@
+// src/app/services/taxa.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SpringService {
+  //private baseUrl = 'https://nsr.dryrun.link/api/v1/taxa';
+  private baseUrl = 'https://www-b.nederlandsesoorten.nl/api/v1/taxa';
+
+  constructor(private http: HttpClient) {}
+
+  public get(query: string): Observable<any> {
+    return this.http.get<any>(query);
+  }
+
+  public get_taxa(name: string): Observable<any> {
+    const url = `${this.baseUrl}?taxonName=${name}`;
+    return new Observable((observer) => {
+      this.get(url).subscribe({
+        next: (data) => {
+          observer.next(data);
+          observer.complete();
+        },
+        error: (err) => console.log(err),
+      });
+    });
+  }
+  public get_taxon(linnaeusId: string, item: string): Observable<any> {
+    const url = `${this.baseUrl}/${linnaeusId}/${item}`;
+    return new Observable((observer) => {
+      this.get(url).subscribe({
+        next: (data) => {
+          observer.next(data);
+          observer.complete();
+        },
+        error: (err) => console.log(err),
+      });
+    });
+  }
+}
