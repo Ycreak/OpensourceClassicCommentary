0d08b94083c74cd8a4084d793193b12603becf97
Luuk Nolden <luuk.nolden@naturalis.nl>
2026-04-11 11:22:32 +0200

    Yes

--- social_media/tweet.sh
+++ social_media/tweet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e;
-source /home/luukie/servers/oscc/social_media/env/bin/activate;
-python3 /home/luukie/servers/oscc/social_media/main.py;
+cd /home/luukie/servers/oscc/social_media/;
+uv run main.py;
