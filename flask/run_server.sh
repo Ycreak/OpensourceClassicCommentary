f00e440de27d091c5facc1a047d9d58509034052
Ycreak <y.creak@hotmail.com>
2026-05-06 17:22:44 +0200

    Uv the production server! (#420)

--- flask/run_server.sh
+++ flask/run_server.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-gunicorn --bind 0.0.0.0:5004 wsgi:app --keyfile /etc/letsencrypt/live/oscc.nolden.biz/privkey.pem --certfile /etc/letsencrypt/live/oscc.nolden.biz/cert.pem
+# Use uv run to execute gunicorn within the project context
+exec uv run gunicorn --bind 0.0.0.0:5003 wsgi:app \
+    --keyfile /etc/letsencrypt/live/oscc.nolden.biz/privkey.pem \
+    --certfile /etc/letsencrypt/live/oscc.nolden.biz/cert.pem
+
