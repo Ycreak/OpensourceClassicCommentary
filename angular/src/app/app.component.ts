259364c7cf11216c23966a204c357b6cce81fcff
sajvanwijk <sebas-vanwijk@outlook.com>
2026-07-05 13:36:28 +0200

    Added a delay to tooltips (please see if you find this nicer as well)

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -2,6 +2,21 @@ import { Component, OnInit } from '@angular/core';
 
 import { ApiService } from '@oscc/services/api.service';
 import { RouterOutlet } from '@angular/router';
+import {
+  MAT_TOOLTIP_DEFAULT_OPTIONS,
+  MatTooltipDefaultOptions,
+  MatTooltipModule,
+} from '@angular/material/tooltip';
+
+/** Custom options the configure the tooltip's default show/hide delays. */
+export const myCustomTooltipDefaults: MatTooltipDefaultOptions = {
+  showDelay: 750,
+  hideDelay: 750,
+  touchendHideDelay: 750,
+  // showDelay: 0,
+  // hideDelay: 0,
+  // touchendHideDelay: 0,
+};
 
 @Component({
   selector: 'app-root',
@@ -9,6 +24,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss'],
   standalone: true,
   imports: [RouterOutlet],
+  providers: [{provide: MAT_TOOLTIP_DEFAULT_OPTIONS, useValue: myCustomTooltipDefaults}],
 })
 export class AppComponent implements OnInit {
   title = 'OpenSourceClassicCommentary';
